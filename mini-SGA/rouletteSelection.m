0697d3dac6fee8b650aa098bb10b8387b9a6c051
Quepas <oneinchman.dev@gmail.com>
2014-06-03 01:03:23 +0200

    Optimization in rouletteSelection (-100ms).

--- mini-SGA/rouletteSelection.m
+++ mini-SGA/rouletteSelection.m
@@ -2,13 +2,7 @@ function selected_genotypes = rouletteSelection(genotypes, select_number)
   selected_genotypes = [];
 
   genotypes_fitness = calculateFitness(genotypes);
-  genotypes_probability = zeros(size(genotypes, 1), 1);
-  genotypes_sum_probability = sum(genotypes_fitness);
-
-  for k=1:length(genotypes_fitness)
-    genotypes_probability(k, :) = genotypes_fitness(k)/genotypes_sum_probability;
-  end
-  genotypes_probability = cumsum(genotypes_probability);
+  genotypes_probability = cumsum(genotypes_fitness./sum(genotypes_fitness));
 
   for i=1:select_number
     idx = genotypes_probability > rand;
